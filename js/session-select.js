8619d1ee64f8de4f92a0a9bed362022e8a53c892
Jakub Nowak <birdt_@tuta.io>
2025-02-09 20:07:38 +0800

    User and session selection rotaries

---
+++ js/session-select.js
@@ -0,0 +1,37 @@
+const sessionsWrapper = document.querySelector('#session-items-wrapper');
+const sessions = document.querySelectorAll('#session');
+let currentSession = 0;
+let isAnimatingSession = false;
+
+function cycleSession(direction) {
+    if (isAnimatingSession) return;
+    
+    const itemWidth = sessions[0].offsetWidth;
+    const oldIndex = currentSession;
+    currentSession = (currentSession + direction + sessions.length) % sessions.length;
+    
+    const animationOffset = direction * itemWidth;
+    
+    sessionsWrapper.style.transform = `translateX(${-oldIndex * itemWidth - animationOffset}px)`;
+    isAnimatingSession = true;
+    
+    setTimeout(() => {
+        sessionsWrapper.style.transition = 'none';
+        sessionsWrapper.style.transform = `translateX(-${currentSession * itemWidth}px)`;
+        void sessionsWrapper.offsetHeight; // Trigger reflow
+        sessionsWrapper.style.transition = 'transform 0.3s ease-in-out';
+        isAnimatingSession = false;
+    }, 300);
+}
+
+function populateSessions() {
+    let sessionObjects = lightdm.sessions;
+    sessionsWrapper.innerHtml = "";
+    sessionObjects.forEach((s) => {
+	let newSession = document.createElement("div");
+	newSession.id = s.key;
+	newSession.className = "item session";
+	newSession.textContent = s.name;
+	usersWrapper.appendChild(newSession);
+    });
+}
---
+++ js/user-select.js
@@ -0,0 +1,37 @@
+const usersWrapper = document.querySelector('#user-items-wrapper');
+const users = document.querySelectorAll('.user');
+let currentUser = 0;
+let isAnimatingUsers = false;
+
+function cycleUser(direction) {
+    if (isAnimatingUsers) return;
+    
+    const itemWidth = users[0].offsetWidth;
+    const oldIndex = currentUser;
+    currentUser = (currentUser + direction + users.length) % users.length;
+    
+    const animationOffset = direction * itemWidth;
+    
+    usersWrapper.style.transform = `translateX(${-oldIndex * itemWidth - animationOffset}px)`;
+    isAnimatingUsers = true;
+    
+    setTimeout(() => {
+        usersWrapper.style.transition = 'none';
+        usersWrapper.style.transform = `translateX(-${currentUser * itemWidth}px)`;
+        void usersWrapper.offsetHeight; // Trigger reflow
+        usersWrapper.style.transition = 'transform 0.3s ease-in-out';
+        isAnimatingUsers = false;
+    }, 300);
+}
+
+function populateUsers() {
+    let userObjects = lightdm.users;
+    usersWrapper.innerHtml = "";
+    userObjects.forEach((u) => {
+	let newUser = document.createElement("div");
+	newUser.id = u.username;
+	newUser.className = "item user";
+	newUser.textContent = u.display_name;
+	usersWrapper.appendChild(newUser);
+    });
+}
