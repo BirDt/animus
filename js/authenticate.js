4e19dff864eb37a12361d3fc7c8af6a75170e680
Jakub Nowak <birdt_@tuta.io>
2025-02-09 20:55:37 +0800

    Remove debug

--- js/authenticate.js
+++ js/authenticate.js
@@ -4,7 +4,6 @@ function bindAuthenticate() {
 	var keycode = e.code || e.key;
 	if(keycode == "Enter"){
 	    const selectedUser = users[currentUser].id;
-	    console.log(`Authenticating ${selectedUser}`);
 
 	    lightdm.authenticate(selectedUser);
 	    return;
@@ -12,13 +11,11 @@ function bindAuthenticate() {
     }
 
     lightdm.show_prompt.connect((text, type) => {
-	console.log(`Responding with ${$("#password-input").value}`);
 	lightdm.respond($("#password-input").value);
     });
     
     lightdm.authentication_complete.connect(() => {
 	const selectedSession = sessions[currentSession].id;
-	console.log(`Starting session ${selectedSession}`)
 	lightdm.start_session(selectedSession);
     });
 }
