973cf5725c76f581017f6ee3518ea08519356e79
Jakub Nowak <birdt_@tuta.io>
2025-02-09 20:45:04 +0800

    Debug auth flow

--- js/authenticate.js
+++ js/authenticate.js
@@ -1,5 +1,6 @@
 function authenticate(){
     const selectedUser = users[currentUser].id;
+    console.log(selectedUser);
 
     lightdm.cancel_authentication();
     lightdm.authenticate(selectedUser);
@@ -7,13 +8,16 @@ function authenticate(){
 
 async function authenticationComplete() {
     const selectedSession = sessions[currentSession].id;
+    console.log(selectedSession);
     
     await lightdm.respond($("#password-input").value);
+    console.log("Response positive");
     lightdm.start_session(selectedSession);
 }
 
 function bindAuthenticate() {
     $("#password-input").onkeypress = function(e) {
+	console.log("Submitting");
 	var keycode = e.code || e.key;
 	if(keycode == "Enter"){
 	    authenticate();
